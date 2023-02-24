a8b7c9dc5ef8a8a51988269f09128a66cbf7908b
Nodman <rewind-pewter.0e@icloud.com>
2023-02-24 18:42:25 +0200

    remove cnfig

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,14 +5,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGhW4UyexupiQs__fAoe1dBbcAVVp2vSs",
-  authDomain: "poker-app-74315.firebaseapp.com",
-  databaseURL:
-    "https://poker-app-74315-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "poker-app-74315",
-  storageBucket: "poker-app-74315.appspot.com",
-  messagingSenderId: "298551651727",
-  appId: "1:298551651727:web:1e7e9499744f1da401effa",
 };
 
 // Initialize Firebase
